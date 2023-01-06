ee31d050a2dc07ae6c74f302b13a2f895465e966
sahanenes <sahanenesss@gmail.com>
2023-01-06 11:50:16 +0300

    last commit

--- flight_app/flight/urls.py
+++ flight_app/flight/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import FlightView
+from .views import FlightView,ReservationView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('flights',FlightView)
+router.register('reservations',ReservationView)
+
 
 urlpatterns = [
     
