6b3b8476acf899693008f066e174c8c0b699b44d
sahanenes <sahanenesss@gmail.com>
2023-01-18 13:29:57 +0300

    docker-compose

--- 017_Docker/backend/tutorial/serializers.py
+++ 017_Docker/backend/tutorial/serializers.py
@@ -4,22 +4,22 @@ from .models import Tutorial
 class TutorialSerializer(serializers.ModelSerializer):
     class Meta:
         model=Tutorial
-        fields="__all__"
+        fields=["title","description"]
 
-class TutorialSerializer(serializers.Serializer):
-    id= serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    description = serializers.CharField()
+# class TutorialSerializer(serializers.Serializer):
+#     id= serializers.IntegerField(read_only=True)
+#     title = serializers.CharField()
+#     description = serializers.CharField()
     
-    def create(self, validated_data):
-        return Tutorial.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Tutorial.objects.create(**validated_data)
     
-    def update(self,instance,validated_data):
-        instance.title = validated_data.get('title',instance.title)
-        instance.description = validated_data.get('description',instance.description)
-        instance.save()
-        return instance
-    def validate(self,data):#?object level validation yapmak için override ediyoruz  
-        if data['title'] == data['description']:
-            raise serializers.ValidationError('Title and description must not be same')
-        return data
+#     def update(self,instance,validated_data):
+#         instance.title = validated_data.get('title',instance.title)
+#         instance.description = validated_data.get('description',instance.description)
+#         instance.save()
+#         return instance
+#     def validate(self,data):#?object level validation yapmak için override ediyoruz  
+#         if data['title'] == data['description']:
+#             raise serializers.ValidationError('Title and description must not be same')
+#         return data
