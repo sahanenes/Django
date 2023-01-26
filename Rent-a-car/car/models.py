43daebcc7e1c9ba185787dd51cb95372ea145cf6
sahanenes <sahanenesss@gmail.com>
2023-01-26 23:41:19 +0300

    add reservation

--- Rent-a-car/car/models.py
+++ Rent-a-car/car/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
 
 GEAR = (
         ("Auto", "Automatic"),
@@ -10,8 +12,28 @@ class Car(models.Model):
     model = models.CharField(max_length=20)
     year = models.SmallIntegerField()
     gear = models.CharField(max_length=6,choices=GEAR)
-    rent_per_day = models.SmallIntegerField()
+    rent_per_day = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        validators=[MinValueValidator(1)]
+    )
     availability = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.brand}/{self.model}-{self.plate_number}"
+
+class Reservation(models.Model):
+    customer = models.ForeignKey(User,on_delete=models.CASCADE,related_name="customers")
+    car = models.ForeignKey(Car,on_delete=models.CASCADE,related_name="cars")
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.customer} reserved {self.car.brand}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['customer','start_date','end_date'],name='user_rent_date'
+            )
+        ]
